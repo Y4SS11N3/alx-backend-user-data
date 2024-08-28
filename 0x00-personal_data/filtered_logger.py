b7d5b167dfd0641442d9a3e058c166c6f8a60abe
YASSINE <contact@yassine.fun>
2024-08-28 16:24:15 -0700

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -84,13 +84,13 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     Create a connection to the database.
 
     Returns:
-        mysql.connector.connection.MySQLConnection: A database connection object.
+        mysql.connector.connection.MySQLConnection: A database connection
+        object.
     """
     username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
     host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.environ.get("PERSONAL_DATA_DB_NAME")
-
     return mysql.connector.connect(
         user=username,
         password=password,

f5869706b642535103c0c21527398490f7e9ba11
YASSINE <contact@yassine.fun>
2024-09-04 13:00:13 -0700

    0x02. Session authentication

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -16,6 +16,18 @@ def view_all_users() -> str:
     return jsonify(all_users)
 
 
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def view_current_user():
+    """ GET /api/v1/users/me
+    Return:
+      - User object JSON represented
+      - 404 if the User ID doesn't exist
+    """
+    if request.current_user is None:
+        abort(404)
+    return jsonify(request.current_user.to_json())
+
+
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
     """ GET /api/v1/users/:id

844988fa1be8b63ef352483ed861a19363aba15d
YASSINE <contact@yassine.fun>
2024-09-04 13:02:19 -0700

    0x02. Session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -59,24 +59,16 @@ def before_request() -> str:
     """
     if auth is None:
         return
-
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/',
-                      '/api/v1/auth_session/login/']
-
+                      '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-
-    if auth.authorization_header(request) is None
-    and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None:
         abort(401)
-
-    current_user = auth.current_user(request)
-    if current_user is None:
+    request.current_user = auth.current_user(request)
+    if request.current_user is None:
         abort(403)
-
-    request.current_user = current_user
 
 
 if __name__ == "__main__":
